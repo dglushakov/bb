3178e64c8bc4cb52893f51caf28b2d2d6cc4f359
Glushakov Denis Vladimirovich <denis.glushakov@office.lamoda.ru>
2019-07-30 17:04:33 +0300

    warning

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        for ($i=1; $i<10; $i++){
+            $user = new User();
+            $user->setUsername('user'.$i);
+            $user->setRoles([
+                'ROLE_USER',
+            ]);
+            $user->setPassword($this->passwordEncoder->encodePassword(
+                $user,
+                $i
+            ));
+            $manager->persist($user);
+        }
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
