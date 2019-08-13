f5112b3203c51375e7ff530e70902dd0e36a5aa4
Glushakov Denis Vladimirovich <denis.glushakov@office.lamoda.ru>
2019-08-13 09:13:03 +0300

    ''

--- src/Controller/Trassir/TrassirDataCollector.php
+++ src/Controller/Trassir/TrassirDataCollector.php
@@ -52,9 +52,9 @@ class TrassirDataCollector extends AbstractController
         $trassirNvrData->setTrassirNvrId($trassirNvr);
         $trassirNvrData->setDateTime(new \DateTime());
 
-        dump($trassirServer);
-        dump($trassirNvr);
-        dd($trassirNvrData);
+        //dump($trassirServer);
+        //dump($trassirNvr);
+        //dd($trassirNvrData);
         $em->persist($trassirNvrData);
         $em->flush();
 
