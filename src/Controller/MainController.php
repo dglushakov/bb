59797563740a4790ff558ad46969d1ed3d394295
Glushakov Denis Vladimirovich <denis.glushakov@lamoda.ru>
2019-09-25 09:21:07 +0300

    couple of statistic rows on home page

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -4,6 +4,10 @@
 namespace App\Controller;
 
 
+use App\Entity\Facility;
+use App\Entity\Safe;
+use App\Entity\TrassirNvr;
+use App\Entity\TrassirNvrData;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,9 +20,52 @@ class MainController extends AbstractController
      */
     public function home(){
         $this->denyAccessUnlessGranted('ROLE_USER');
+        $summaryData=[];
+
+        $facilityRepo = $this->getDoctrine()->getRepository(Facility::class);
+        $facilities = $facilityRepo->findAll();
+        $facilityCount = count($facilities);
+        $summaryData['facilityCount'] = $facilityCount;
+
+        $safesRepo = $this->getDoctrine()->getRepository(Safe::class);
+        $equippedWithSafes=0;
+        foreach ($facilities as $facility) {
+            if ($safesRepo->findBy(['facility'=>$facility])){
+                $equippedWithSafes++;
+            }
+        }
+        $summaryData['equippedWithSafes']  = $equippedWithSafes;
+
+        $trassirNnrRepo = $this->getDoctrine()->getRepository(TrassirNvr::class);
+        $trassirNvrList = $trassirNnrRepo->findAll();
+        $summaryData['trassirNvrCount'] = count($trassirNvrList);
+        $equippedWithTrassir=0;
+        foreach ($facilities as $facility) {
+            if ($trassirNnrRepo->findBy(['facility'=>$facility])){
+                $equippedWithTrassir++;
+            }
+        }
+        $summaryData['equippedWithTrassir']  = $equippedWithTrassir;
+
+        $trassirDataRepo = $this->getDoctrine()->getRepository(TrassirNvrData::class);
+        $trassirHealth=[];
+        foreach ($trassirNvrList as $trassirNvr){
+            $trassirHealth[$trassirNvr->getId()] =
+                $trassirDataRepo->findOneBy(['trassirNvrId'=>$trassirNvr->getId()],['dateTime'=>'DESC']);
+        }
+
+        $trassirDataRepo = $this->getDoctrine()->getRepository(TrassirNvrData::class);
+        $trassirNvrOnline=0;
+        foreach ($trassirNvrList as $trassirNvr){
+            $health = $trassirDataRepo->findOneBy(['trassirNvrId'=>$trassirNvr->getId()],['dateTime'=>'DESC']);
+            if ($health and $health->getHealth()['network']==1 ) {
+                $trassirNvrOnline++;
+            }
+        }
+        $summaryData['trassirNvrOnline']  = $trassirNvrOnline;
 
         return $this->render('home.html.twig', [
-            'data'=>'home',
+            'data'=>$summaryData,
         ]);
     }
 
